2687ee4efde272e8088cc88d1565f062f4f608ef
lmsyoofch <lmsyoofch@gmail.com>
2026-02-25 20:04:19 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1424,7 +1424,7 @@ const events = [
   notes: "Start time 14:00 (GMT+7)",
   notes_th: "เริ่ม 14:00 น. (GMT+7)",
   notes_zh: "开始时间 14:00（GMT+7）",
-  hashtags: ["#LMSYLuckyMoment"],
+  hashtags: ["#LMSYLuckyMoment,"#Downy", "#DownyThailand", "#BigC"],
   tags: ["LMSY", "Brand"]
 },
       {
