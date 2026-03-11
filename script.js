d6a243bb61acbf33f197e45977fe4d8b401360b2
lmsyoofch <lmsyoofch@gmail.com>
2026-03-11 21:54:04 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1453,7 +1453,7 @@ const events = [
   notes: "Official teaser premiere at 20:00 (GMT+7)",
   notes_th: "ปล่อยทีเซอร์อย่างเป็นทางการ เวลา 20:00 น. (GMT+7)",
   notes_zh: "官方预告发布，时间 20:00（GMT+7）",
-  hashtags: ["Hometown Romance Teaser\n\n#HometownRomanceTeaser"],
+  hashtags: ["HOMETOWN ROMANCE TEASER\n\n#HometownRomanceTeaser"],
   tags: ["LMSY", "Drama", "Teaser"]
 },
 {
